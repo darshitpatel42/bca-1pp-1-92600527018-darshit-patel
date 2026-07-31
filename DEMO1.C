0212a4a47a5c54062b790dee45137546d51c7f8a
darshitpatel42 <darshitpateldarshitpatel42@gmail.com>
2026-07-31 10:04:37 +0530

    Add files via upload

---
+++ DEMO1.C
@@ -0,0 +1,12 @@
+//write a program to display good morning
+//darshit bhagiya
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("Good morning");
+	printf("darshit bhagiya");
+	getch();
+
+}
---
+++ DEMO2.C
@@ -0,0 +1,17 @@
+//wap which displays sum of two number
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,z;	//variable declaratiion
+	clrscr();
+	x=500;
+	y=300;
+	z=x+y;
+	printf("\n %d is the answer",z);
+	printf("\n %d + %d = %d",x,y,z);
+
+	getch();
+
+}
